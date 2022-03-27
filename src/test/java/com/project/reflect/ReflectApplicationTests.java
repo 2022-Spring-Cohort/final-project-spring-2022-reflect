0ff867d5dd8f03d8dc18ee0d3bc8da70ba86aa13
JSwys <jswysgood@gmail.com>
2022-03-27 15:53:02 -0400

    added dependencies and setup project

---
+++ src/test/java/com/project/reflect/ReflectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.reflect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
