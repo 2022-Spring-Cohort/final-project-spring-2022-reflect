b886182875b940472649b17df0cb68aa36dc93d5
HungryManSteve <mliddy09@gmail.com>
2022-04-04 13:00:31 -0400

    Populated populator with high-quality articles
    Took 58 minutes

--- src/main/java/com/project/reflect/Model/Article.java
+++ src/main/java/com/project/reflect/Model/Article.java
@@ -9,17 +9,37 @@ public class Article {
     private String author;
     private String content;
     private String title;
+    private String picture;
+    private String link;
+
 
     public Article(){
 
     }
 
-    public Article(String author, String content, String title){
+    public Article(String title, String author, String picture, String content, String link){
         this.author = author;
-        this.content = content;
         this.title = title;
+        this.content = content;
+        this.picture = picture;
+        this.link = link;
+
+    }
+    public String getPicture() {
+        return picture;
     }
 
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
     public String getAuthor() {
         return author;
     }
