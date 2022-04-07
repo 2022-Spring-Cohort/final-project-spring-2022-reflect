d625bca1c00529c6c4fff5d6d891b65057503843
DeJuanSpencer <dejuanspencer@gmail.com>
2022-04-07 14:56:45 -0400

    Added javascript to mindset pages why and wins

--- front-end/js/mindset.js
+++ front-end/js/mindset.js
@@ -5,9 +5,3 @@ cards.forEach((card) => {
     card.classList.toggle("is-flipped");
   });
 });
-const test = document.querySelector(".fortest");
-console.log(test);
-
-const mindsetCard = document.createElement("div");
-mindsetCard.innerText = "hello";
-test.appendChild(mindsetCard);
--- front-end/js/mindsetWhyView.js
+++ front-end/js/mindsetWhyView.js
@@ -6,6 +6,48 @@ export default function mindsetWhy(mindsetCardsWhy) {
   ${mindsetCardsWhy
     .map((mindsetCardWhy) => {
       return `
+
+
+      <section class="mindset__card mindset__intro">
+      <h1>Mindset Trainer</h1>
+
+      <p>
+        A growth mindset can be trained by all of us. Below are prompts to guide
+        you into further developing a growth mindset.
+      </p>
+    </section>
+
+    <section class="mindset__card mindset__why">
+      <h2>Why are you doing this?</h2>
+      <p>
+        Think about the moments where you struggled and succeeded at the end.
+        Remind yourself how you overcame those issues. This can be used to
+        encourage yourself because you've won over difficulty before and can do
+        it again.
+      </p>
+    </section>
+    <section class="mindset__focuses">
+      <section class="mindsets___growth">
+        <h2>Here are some cards to input your whys. Repetition is key.</h2>
+        <form>
+          <textarea
+            id="why"
+            name="why"
+            rows="5"
+            columns="20"
+            placeholder="What do you want to do?"
+          ></textarea>
+
+          <textarea
+            id="why__details"
+            name="why__details"
+            rows="5"
+            columns="20"
+            placeholder="Why does this matter to you?"
+          ></textarea>
+          <input type="submit" />
+        </form>
+
   <div class="card">
   <div class="card__inner">
   <input id="id" type="hidden" name="id" value="${mindsetCardWhy.id}">
@@ -47,19 +89,6 @@ export default function mindsetWhy(mindsetCardsWhy) {
     })
     .join("")}
   </div>
-
-
-
-  <div class="card-input">
-  <h2 class="new-card">Add A New Card</h2>
-  
-  <input type="text" class="card-front-input" placeholder="Front of Card" />
-  <input type="text" class="card-back-input" placeholder="Back of Card" />
-  <input type="text" class="card-image-input" placeholder="Enter image file" />
-  
-  
-  <button class="add-new-card-button">Add Card</button>
-  
 </div>    
   `;
 }
