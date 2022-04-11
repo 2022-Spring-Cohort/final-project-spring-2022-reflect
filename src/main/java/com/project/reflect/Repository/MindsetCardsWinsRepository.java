01a1b8c53ccc64bc8f022b817257bc71f08f49ee
DeJuanSpencer <dejuanspencer@gmail.com>
2022-04-11 10:22:19 -0400

    Added mindset cards repos

---
+++ src/main/java/com/project/reflect/Repository/MindsetCardsWinsRepository.java
@@ -0,0 +1,13 @@
+
+package com.project.reflect.Repository;
+
+import com.project.reflect.Model.MindsetCardsWins;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface MindsetCardsWinsRepository extends CrudRepository<MindsetCardsWins, Long> {
+
+}
+
+
