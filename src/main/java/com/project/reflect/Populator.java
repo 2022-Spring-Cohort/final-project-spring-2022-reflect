398bf18d70675f4a9c480243d3217e415713f9aa
BenjaminCorrigan <corriganbenjamin>
2022-04-11 13:15:59 -0400

    changes

--- src/main/java/com/project/reflect/Populator.java
+++ src/main/java/com/project/reflect/Populator.java
@@ -83,17 +83,17 @@ public class Populator implements CommandLineRunner {
                 "https://www.mindful.org/tara-brach-rain-mindfulness-practice/",topicAnxiety);
         articleRepo.save(article2);
 
-        Article article3 = new Article("Five Ways Mindfulness Meditation Is Good for Your Health", "Jill Suittie", "../front-end/images/tibetan-bowl.png",
+        Article article3 = new Article("Five Ways Mindfulness Meditation Is Good for Your Health", "Jill Suittie", "./images/tibetan-bowl.png",
                 "According to thousands of years of tradition, Buddhists meditate to understand themselves and their connections to all beings. By doing so, they hope to be released from suffering and ultimately gain enlightenment.",
                 "https://greatergood.berkeley.edu/article/item/five_ways_mindfulness_meditation_is_good_for_your_health", topicMindfulness);
         articleRepo.save(article3);
 
-        Article article4 = new Article("Mindfulness Makeover: How To 'Just Be' No Matter What", "Sarah Jeanne Browne", "./front-end/images/tibetan-bowl.png",
+        Article article4 = new Article("Mindfulness Makeover: How To 'Just Be' No Matter What", "Sarah Jeanne Browne", "./images/tibetan-bowl.png",
                 "Stressed and struggling? If you are having a hard day, and you can’t get out of your own head, mindfulness may be for you. You can’t always turn the mind off. That’s not what mindfulness is...",
         "https://www.forbes.com/sites/womensmedia/2021/01/21/mindfulness-makeover-how-to-just-be-no-matter-what/", topicMeditation);
         articleRepo.save(article4);
 
-        Article article5 = new Article("6 Ways to Practice Mindful Eating", "Christopher Willard", "../front-end/images/tibetan-bowl.png",
+        Article article5 = new Article("6 Ways to Practice Mindful Eating", "Christopher Willard", "./images/tibetan-bowl.png",
                 "Eating as mindfully as we do on retreat or in a mindfulness course is not realistic for many of us, especially with families, jobs, and the myriad distractions around us. This is not to mention that our friends, family and...",
                 "https://www.mindful.org/6-ways-practice-mindful-eating/", topicMindfulness);
         articleRepo.save(article5);
